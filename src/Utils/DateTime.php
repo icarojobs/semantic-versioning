4428c63c7891c3f292fd11d890b04211f3c55c37
Tio Jobs <icarojobsoficial@gmail.com>
2023-09-28 07:21:05 -0300

    fix: fix return of DateTime class

--- src/Utils/DateTime.php
+++ src/Utils/DateTime.php
@@ -6,7 +6,7 @@ namespace Tiojobs\SemanticVersioning\Utils;
 
 class DateTime
 {
-    public static function now(): int
+    public static function now(): string
     {
         return date('d/m/Y H:i:s');
     }
